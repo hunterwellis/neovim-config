280cb270aafb87bba694f7f799c84ffef42e28f9
Hunter Ellis <elliswhunter@gmail.com>
2025-08-26 16:35:59 -0400

    fixed typst

--- lua/plugins/typstpreview.lua
+++ lua/plugins/typstpreview.lua
@@ -3,7 +3,4 @@ return {
   lazy = false, -- or ft = 'typst'
   version = '1.*',
   opts = {}, -- lazy.nvim will implicitly calls `setup {}`
-  dependencies_bin = {
-    ['tinymist'] = 'tinymist',
-  },
 }
